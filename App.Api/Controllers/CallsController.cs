d3f13f98a5fc443859b64d59dc820e5022aca003
Robert Dudiver <rdudiver@andreani.com>
2023-10-26 15:10:57 -0300

    feat signal ar

--- App.Api/Controllers/CallsController.cs
+++ App.Api/Controllers/CallsController.cs
@@ -1,9 +1,10 @@
 ﻿
-using App.Api.Application.UseCase.V1.RestaurantOperation.Command.Create;
-using App.Api.Application.UseCase.V1.RestaurantOperation.Command.Delete;
-using App.Api.Application.UseCase.V1.RestaurantOperation.Command.Update;
-using App.Api.Application.UseCase.V1.RestaurantOperation.Queries.GetList;
-using App.Api.Application.UseCase.V1.RestaurantOperation.Queries.GetSearch;
+using App.Api.Application.UseCase.V1.CallsOperation.Command.Create;
+using App.Api.Application.UseCase.V1.CallsOperation.Command.Delete;
+using App.Api.Application.UseCase.V1.CallsOperation.Command.Update;
+using App.Api.Application.UseCase.V1.CallsOperation.Queries.GetList;
+using App.Api.Application.UseCase.V1.CallsOperation.Queries.GetSearch;
+
 using App.Api.Common;
 using App.Core.Domain.DTO;
 using App.Core.Domain.Entities;
@@ -12,37 +13,37 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace App.Api.Controllers
 {
-    public class RestaurantController : ApiControllerBase
+    public class CallsController : ApiControllerBase
     {
         [HttpPost]
-        [ProducesResponseType(typeof(CreateRestaurantCommand), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(CreateCallsHubCommand), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(List<Notify>), StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Create(CreateRestaurantCommand body) => Result(await Mediator.Send(body));
+        public async Task<IActionResult> Create(CreateCallsHubCommand body) => Result(await Mediator.Send(body));
         [HttpPut]
-        [ProducesResponseType(typeof(UpdateRestaurantCommand), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(UpdateCallsCommand), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(List<Notify>), StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Update(UpdateRestaurantCommand body) => Result(await Mediator.Send(body));
+        public async Task<IActionResult> Update(UpdateCallsCommand body) => Result(await Mediator.Send(body));
 
         [HttpDelete]
-        [ProducesResponseType(typeof(DeleteRestaurantCommand), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(DeleteCallsCommand), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(List<Notify>), StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Delete(DeleteRestaurantCommand body) => Result(await Mediator.Send(body));
+        public async Task<IActionResult> Delete(DeleteCallsCommand body) => Result(await Mediator.Send(body));
 
 
 
         [HttpGet]
-        [ProducesResponseType(typeof(List<Restaurant>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<Calls>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(List<Notify>), StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Get() => Result(await Mediator.Send(new ListRestaurant()));
+        public async Task<IActionResult> Get() => Result(await Mediator.Send(new ListCalls()));
 
 
 
         [HttpGet("Search")]
-        [ProducesResponseType(typeof(List<Restaurant>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<Calls>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(List<Notify>), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Search([FromQuery] string SearchText)
         {
-            var searchRequest = new SearchRestaurant
+            var searchRequest = new SearchCalls
             {
                 SearchText = SearchText
             };
@@ -53,4 +54,5 @@ namespace App.Api.Controllers
 
 
     }
+
 }
